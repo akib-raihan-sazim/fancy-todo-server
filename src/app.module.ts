da0e0c77c9c1ec2967b2135964688e47122c4add
Akib Raihan <akib.raihan@sazim.io>
2024-08-02 14:32:06 +0600

    feat(FTDA): Config file for connecting DB chore(FTDA): Followed file-structure convenience. chore(FTDA): Removed files which is unnecssary now.

---
+++ src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from "@nestjs/common";
+import { MikroOrmModule } from "@mikro-orm/nestjs";
+
+import config from "./db/mikro-orm.config";
+
+@Module({
+  imports: [MikroOrmModule.forRoot(config)],
+})
+export class AppModule {}
---
+++ src/db/mikro-orm.config.ts
@@ -0,0 +1,17 @@
+import { Options } from "@mikro-orm/core";
+import { PostgreSqlDriver } from "@mikro-orm/postgresql";
+import { TsMorphMetadataProvider } from "@mikro-orm/reflection";
+
+const config: Options = {
+  driver: PostgreSqlDriver,
+  host: "localhost",
+  port: 5432,
+  user: "postgres",
+  password: "1234",
+  dbName: "to-do",
+  entities: ["dist/**/*.entity.js"],
+  entitiesTs: ["src/**/*.entity.ts"],
+  metadataProvider: TsMorphMetadataProvider,
+};
+
+export default config;
